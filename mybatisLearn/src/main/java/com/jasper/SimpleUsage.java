acdfbde1661a398d8353e79c083898d95894922c
jasper <2190320413@qq.com>
2025-04-01 19:38:19 +0800

    jdbc SimpleUsage and mybatis simple usage

---
+++ mybatisLearn/src/main/java/com/jasper/SimpleUsage.java
@@ -0,0 +1,25 @@
+package com.jasper;
+
+import com.jasper.mapper.SystemNoticeMapper;
+import com.jasper.pojo.SystemNotice;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SimpleUsage {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            SystemNoticeMapper mapper = session.getMapper(SystemNoticeMapper.class);
+            SystemNotice systemNotice = mapper.selectSystemNoticeById(1);
+            System.out.println(systemNotice.getTitle());
+        }
+    }
+}
---
+++ mybatisLearn/src/main/java/com/jasper/mapper/SystemNoticeMapper.java
@@ -0,0 +1,8 @@
+package com.jasper.mapper;
+
+import com.jasper.pojo.SystemNotice;
+
+public interface SystemNoticeMapper {
+
+    SystemNotice selectSystemNoticeById(Integer id);
+}
